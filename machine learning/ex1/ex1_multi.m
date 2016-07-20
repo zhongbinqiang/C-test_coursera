f11e461fe3199b88a785bc298ab56a406e7155d2
zhongbinqiang <zhongbinqiang@gmail.com>
2016-07-20 09:31:18 +0800

    finished week2, begin week3

--- machine learning/ex1/ex1_multi.m
+++ machine learning/ex1/ex1_multi.m
@@ -115,8 +115,7 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-price = theta + 1650*theta(2) +3*theta(3);
+price = theta(1) + (1650-mu(1))*theta(2)/sigma(1) + (3-mu(2))*theta(3)/sigma(2); % You should change this
 
 % ============================================================
 
@@ -160,7 +159,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = theta(1) + theta(2)*1650 + theta(3)*3; % You should change this
 
 
 % ============================================================
